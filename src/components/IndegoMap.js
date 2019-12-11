c8f40f87726e35c4140e5dee7fe1dea2bfd0bf72
Sergio Cabral <sergioantonio60@gmail.com>
2019-12-11 16:20:27 -0500

    Replaces HTML with Overlay Comp on IndegoMap

--- src/components/IndegoMap.js
+++ src/components/IndegoMap.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react'
 import WeatherInfo from './WeatherInfo'
+import Overlay from './Overlay'
 
 class IndegoMap extends Component {
+
   render() {
     return (
       <main>
         <WeatherInfo />
         <div id="map-container"></div>
-        <div id="overlay">
-          <div id="popup">
-              <div id="close" className="btn btn-danger">X</div>
-              <h2 id="station-name">Station</h2>
-              <p><strong>Bikes:</strong> <span id="bikes-available"></span> available</p>
-              <p><strong>Docks:</strong> <span id="docks-available"></span> available</p>
-              <p><strong>Address:</strong> <span id="station-address"></span></p>
-          </div>
-        </div>
+        <Overlay />
       </main>
     )
   }
