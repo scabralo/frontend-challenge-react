37d28c37dedcd31d04742540e2db63ae098ec522
Sergio Cabral <sergioantonio60@gmail.com>
2019-12-11 16:15:43 -0500

    Implements Footer Comp on App

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header'
 import IndegoMap from './IndegoMap'
+import Footer from './Footer'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <IndegoMap />
+      <Footer />
     </div>
   );
 }
