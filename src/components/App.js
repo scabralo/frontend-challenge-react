a8e8c070d1d3c4025f0300d9ce07d5b1d27f2e6f
Sergio Cabral <sergioantonio60@gmail.com>
2019-12-11 16:15:27 -0500

    Implements IndegoMap Comp on App

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header'
+import IndegoMap from './IndegoMap'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <IndegoMap />
     </div>
   );
 }
